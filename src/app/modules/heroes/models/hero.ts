945e984d30dfa8853ad9a635ea480b39cc13c72e
Islam <islam.youniss90@gmail.com>
2022-03-26 23:51:02 +0300

    first commit

---
+++ src/app/modules/heroes/models/hero.ts
@@ -0,0 +1,8 @@
+export interface Hero {
+  name: string;
+  mobile: string;
+  email: string;
+  date: string;
+  country: string;
+  company: string;
+}
---
+++ src/app/modules/shared/components/countries-dropdown/countries-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-countries-dropdown',
+  templateUrl: './countries-dropdown.component.html',
+  styleUrls: ['./countries-dropdown.component.scss']
+})
+export class CountriesDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
